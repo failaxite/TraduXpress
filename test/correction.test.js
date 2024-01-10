4ae92f8b0de767b9e4ed2f74f61a6f38f94a5406
Felix/failaxite <failaxite.don@gmail.com>
2024-01-11 00:00:02 +0100

    fixing try catch for correction

--- test/correction.test.js
+++ test/correction.test.js
@@ -1,6 +1,12 @@
 const correctSpelling = require('../src/correction');
 
 test('corrige l\'orthographe d\'un texte avec succès', () => {
-    const result = correctSpelling('baonjous');
-    console.log(result);
+    try {
+        const textToCorrect = 'baonjous';
+        const result = correctSpelling(textToCorrect);
+        console.log(result);
+        expect(result).toBe('baonjou');
+    } catch (error) {
+        console.error('Erreur de correction d\'orthographe inattendue :', error);
+    }
 });
