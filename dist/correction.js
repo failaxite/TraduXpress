ece5f8d54c3c510a76a1ce5c6f893e7dd18ce9b9
Felix/failaxite <failaxite.don@gmail.com>
2024-01-10 16:46:10 +0100

    change ci/cd file

---
+++ dist/correction.js
@@ -0,0 +1,23 @@
+const natural = require('natural');
+
+/**
+ * Fonction de normalisation d'un texte.
+ *
+ * @param {string} text - Texte à normaliser.
+ * @returns {string} Texte normaliser.
+ */
+function correctSpelling(text) {
+    const tokenizer = new natural.WordTokenizer();
+    const words = tokenizer.tokenize(text);
+
+    const correctedWords = words.map(word => {
+        const stemmedWord = natural.PorterStemmer.stem(word);
+        const correctedWord = natural.JaroWinklerDistance(word, stemmedWord) > 0.7 ? stemmedWord : word;
+
+        return correctedWord;
+    });
+
+    return correctedWords.join(' ');
+}
+
+module.exports = correctSpelling;
